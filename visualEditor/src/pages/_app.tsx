e36e1434be7f165979704f53e2b1082b9e3aced1
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-08-03 16:55:49 +0200

    add html to image

--- visualEditor/src/pages/_app.tsx
+++ visualEditor/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { theme } from '../theme';
+import Script from 'next/script';
 
 const MyApp = ({ pageProps, Component }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
+      <Script src="https://unpkg.com/html-to-image@1.11.11/dist/html-to-image.js" />
       <Component {...pageProps} />
     </ChakraProvider>
   );

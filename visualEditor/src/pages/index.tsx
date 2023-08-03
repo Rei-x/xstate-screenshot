027fdd9cbde81f833f266b3b7625089b3293d3bf
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-08-03 18:14:14 +0200

    add preview

--- visualEditor/src/pages/index.tsx
+++ visualEditor/src/pages/index.tsx
@@ -1,8 +1,91 @@
-import { Divider, Flex, Stack, Text, IconButton } from '@chakra-ui/react';
-import React from 'react';
+import {
+  Divider,
+  Flex,
+  Stack,
+  Text,
+  IconButton,
+  Image,
+  Textarea,
+  FormControl,
+  FormLabel,
+  Box,
+  Button,
+  HStack,
+  Link,
+  FormHelperText,
+  VStack,
+} from '@chakra-ui/react';
+import React, { useEffect } from 'react';
 import { FaGithub } from 'react-icons/fa';
+import * as parse from '@xstate/machine-extractor';
+import lzstring from 'lz-string';
 
 export const Index = () => {
+  const [fileContent, setFileContent] =
+    React.useState<string>(`import { createMachine } from 'xstate';
+
+  export const machine = createMachine({
+    id: 'machine',
+    initial: 'idle',
+    states: {
+      idle: {
+        on: {
+          FETCH: 'loading',
+        },
+      },
+      loading: {
+        on: {
+          RESOLVE: 'success',
+          REJECT: 'failure',
+        },
+      },
+      success: {
+        type: 'final',
+      },
+      failure: {
+        type: 'final',
+      },
+    },
+  });`);
+  const [error, setError] = React.useState<string>('');
+  const [screenshotUrl, setScreenshotUrl] = React.useState<string>('');
+  const [machineUrl, setMachineUrl] = React.useState<string>('');
+
+  useEffect(() => {
+    try {
+      const parsed = parse.extractMachinesFromFile(fileContent);
+
+      const machine = parsed?.machines.at(0);
+
+      if (!machine) {
+        return;
+      }
+      const compressed = lzstring.compressToEncodedURIComponent(
+        JSON.stringify(machine?.toConfig()),
+      );
+
+      if (compressed.length === 0) {
+        return;
+      }
+
+      const machineUrl = `https://xstate-screenshot.vercel.app/machine/${compressed}`;
+
+      const params = new URLSearchParams({
+        url: machineUrl,
+        element: '#canvas > div:nth-child(1) > div:nth-child(2)',
+      });
+      const screenshotApiUrl = `https://browser-screenshots.vercel.app/api?${params}`;
+
+      setError('');
+      setMachineUrl(machineUrl);
+      setScreenshotUrl(screenshotApiUrl);
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e.message);
+      }
+    }
+  }, [fileContent]);
+
   return (
     <Stack py="10" px="20" overflow="auto" maxH="100vh">
       <div>
@@ -28,6 +111,32 @@ export const Index = () => {
         </Flex>
         <Divider />
       </div>
+      <Box maxW="container.md">
+        <FormControl>
+          <FormLabel>Paste machine file</FormLabel>
+          <Textarea
+            value={fileContent}
+            onChange={(e) => setFileContent(e.target.value)}
+            minH="lg"
+            defaultValue={``}
+            placeholder={``}
+          />
+          <FormHelperText>
+            {error ? <Text color="red.500">{error}</Text> : null}
+          </FormHelperText>
+        </FormControl>
+        <VStack my={2}>
+          <Link href={screenshotUrl}>Screenshot url</Link>
+          <Link href={machineUrl}>Machine URL (preview)</Link>
+        </VStack>
+        <Box>
+          <Image
+            src={screenshotUrl}
+            alt="screenshot"
+            fallbackSrc="https://miro.medium.com/v2/resize:fit:3840/1*oQXT3LKxRHELzgutvn1XZg.png"
+          />
+        </Box>
+      </Box>
     </Stack>
   );
 };
